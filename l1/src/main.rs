48d8d1be765ba803fa61cd77d2269f39fbeacdb2
Spiryd <neumann.maks@gmail.com>
2023-10-10 08:46:23 +0200

    file ingestion implemented

--- l1/src/main.rs
+++ l1/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use rayon::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let file = std::fs::read(file_path).unwrap();
+    //println!("{:?}", file.len());
+    let mut occurences: Vec<u32> =  vec![0; u8::MAX as usize];
+    for symbol in file {
+        occurences[symbol as usize] += 1;
+    }
+    dbg!(&occurences);
 }
