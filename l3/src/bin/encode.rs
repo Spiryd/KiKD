8dfa9c95bbdce863fdba2a43e4830df76d8df60c
Spiryd <neumann.maks@gmail.com>
2023-11-19 12:57:16 +0100

    final l3

--- l3/src/bin/encode.rs
+++ l3/src/bin/encode.rs
@@ -2,34 +2,40 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 
-use l3::{*, CodingType::*};
+use l3::{CodingType::*, *};
 
 fn main() {
-        // Take in and vaidate input
-        let args: Vec<String> = env::args().collect();
-        if args.len() < 3 {
-            panic!("Wrong argument count")
-        }
-        let encoding_type = match args.get(3) {
-            Some(enc_str) => {
-                match enc_str.as_str() {
-                    "gamma" => GAMMA,
-                    "delta" => DELTA,
-                    "fib" => FIB,
-                    _ => OMEGA,
-                }
-            },
-            None => OMEGA,
-        };
-        let file_path = args.get(1).unwrap();
-        let output_file_path = args.get(2).unwrap();
-        let file = std::fs::read(file_path).unwrap();
+    // Take in and vaidate input
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        panic!("Wrong argument count")
+    }
+    let encoding_type = match args.get(3) {
+        Some(enc_str) => match enc_str.as_str() {
+            "gamma" => GAMMA,
+            "delta" => DELTA,
+            "fib" => FIB,
+            _ => OMEGA,
+        },
+        None => OMEGA,
+    };
+    let file_path = args.get(1).unwrap();
+    let output_file_path = args.get(2).unwrap();
+    let file = std::fs::read(file_path).unwrap();
 
-        println!("entropy: {:?}", entropy(&file));
+    println!("encodee size: {:?} B", file.len());
+    println!("encodee entropy: {:?}", entropy(&file));
 
-        let encoder = Encoder::new(encoding_type);
-        let encoded = encoder.encode( &file, true);
+    let encoder = Encoder::new(encoding_type);
+    let encoded = encoder.encode(&file, true);
+    let encoded_bytes = encoded.to_bytes();
+    println!("encoding size: {:?} B", encoded_bytes.len());
+    println!("encoding entropy: {:?}", entropy(&encoded_bytes));
+    println!(
+        "compression ratio: {:?}",
+        file.len() as f64 / encoded_bytes.len() as f64
+    );
 
-        let mut output = File::create(output_file_path).unwrap();
-        output.write_all(&encoded.to_bytes()).unwrap();
+    let mut output = File::create(output_file_path).unwrap();
+    output.write_all(&encoded_bytes).unwrap();
 }
--- l3/src/bin/run_test_suite.rs
+++ l3/src/bin/run_test_suite.rs
@@ -1,3 +1,40 @@
+use l3::{CodingType::*, *};
+use std::time::Instant;
+
 fn main() {
-    todo!()
+    // Our tests
+    let test_files = vec![
+        "test_cases/test1.bin",
+        "test_cases/test2.bin",
+        "test_cases/test3.bin",
+        "test_cases/pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt",
+        "test_cases/bee_movie_test_suite/bee_movie_script_x12.txt",
+        "test_cases/bee_movie_test_suite/bee_movie_script_x12.doc",
+        "test_cases/bee_movie_test_suite/bee_movie_script_x12.pdf",
+        "test_cases/bee_movie_test_suite/beemovie.jpg",
+        "test_cases/bee_movie_test_suite/beemovie.mp4",
+    ];
+    for coding in [GAMMA, DELTA, OMEGA, FIB] {
+        for file_path in &test_files {
+            let file = std::fs::read(file_path).unwrap();
+            println!("file: {:?}", &file_path);
+            println!("encoding: {:?}", &coding);
+            println!("encodee size: {:?} B", file.len());
+            println!("encodee entropy: {:?}", entropy(&file));
+        
+            let encoder = Encoder::new(coding);
+            let now = Instant::now();
+            let encoded = encoder.encode(&file, false);
+            let elapsed = now.elapsed();
+            let encoded_bytes = encoded.to_bytes();
+            println!("encoding size: {:?} B", encoded_bytes.len());
+            println!("encoding entropy: {:?}", entropy(&encoded_bytes));
+            println!(
+                "compression ratio: {:?}",
+                file.len() as f64 / encoded_bytes.len() as f64
+            );
+            println!("Elapsed: {:.2?}", elapsed);
+            println!("\n");
+        }
+    }
 }
